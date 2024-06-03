c9ec0728f89a10a6e0046b5ce32c8fb7b6c90ac6
tw1nkl2 <149481966+tw1nkl2@users.noreply.github.com>
2024-06-03 11:17:52 +0200

    second commit

---
+++ EBookClient/TCPClient.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EBookClient
+{
+    public class TCPClient
+    {
+        private string serverIp;
+        private int serverPort;
+        private TcpClient client;
+        private NetworkStream stream;
+
+        public TCPClient(string ip, int port)
+        {
+            serverIp = ip;
+            serverPort = port;
+            client = new TcpClient();
+        }
+
+        public bool Connect()
+        {
+            try
+            {
+                client.Connect(serverIp, serverPort);
+                stream = client.GetStream();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Error connecting to server: " + ex.Message);
+                return false;
+            }
+        }
+
+        public void SendMessage(string message)
+        {
+            if (stream == null || !client.Connected)
+            {
+                //Console.WriteLine("Not connected to server.");
+                return;
+            }
+
+            byte[] data = Encoding.ASCII.GetBytes(message);
+            stream.Write(data, 0, data.Length);
+
+            byte[] responseData = new byte[256];
+            int bytes = stream.Read(responseData, 0, responseData.Length);
+            string response = Encoding.ASCII.GetString(responseData, 0, bytes);
+            Console.WriteLine("Received: " + response);
+        }
+
+        public void Disconnect()
+        {
+            if (stream != null)
+                stream.Close();
+            if (client != null)
+                client.Close();
+        }
+    }
+}
---
+++ EBookServer/TCPServer.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EBookServer
+{
+    public class TCPServer
+    {
+        private TcpListener listener;
+        private bool isRunning;
+
+        public TCPServer(string ip, int port)
+        {
+            listener = new TcpListener(IPAddress.Parse(ip), port);
+        }
+
+        public void Start()
+        {
+            listener.Start();
+            isRunning = true;
+            //Console.WriteLine("Server started...");
+
+            while (isRunning)
+            {
+                try
+                {
+                    TcpClient client = listener.AcceptTcpClient();
+                    //Console.WriteLine("Client connected...");
+
+                    Thread clientThread = new Thread(HandleClient);
+                    clientThread.Start(client);
+                }
+                catch (Exception ex)
+                {
+                    //Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+        }
+
+        private void HandleClient(object obj)
+        {
+            TcpClient client = (TcpClient)obj;
+            NetworkStream stream = client.GetStream();
+            byte[] buff = new byte[256];
+            int bytesRead;
+
+            try
+            {
+                while ((bytesRead = stream.Read(buff, 0, buff.Length)) != 0)
+                {
+                    string message = Encoding.ASCII.GetString(buff, 0, bytesRead);
+                    //Console.WriteLine("Received: " + message);
+
+                    string response = "Server received: " + message;
+                    byte[] responseData = Encoding.ASCII.GetBytes(response);
+                    stream.Write(responseData, 0, responseData.Length);
+                    //Console.WriteLine("Sent: " + response);
+                }
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Client connection error: " + ex.Message);
+            }
+            finally
+            {
+                stream.Close();
+                client.Close();
+            }
+        }
+
+        public void Stop()
+        {
+            isRunning = false;
+            listener.Stop();
+            //Console.WriteLine("Server stopped...");
+        }
+    }
+}
